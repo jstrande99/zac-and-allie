972a28f3b398dbd8b46cb3f25a8af7898c3d610a
Jordan Strande <jstrande@rams.colostate.edu>
2023-03-03 01:23:55 -0700

    Created base log in page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { auth } from './FirebaseConfig';
+import Login from './Login';
+import Social from './Social';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Social /> : <Navigate to="/login" />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+      </Routes>
+    </Router>
   );
 }
 
