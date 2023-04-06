3017bfa5cf7881d194e073e400a3d928da08b19b
Jordan Strande <jstrande@rams.colostate.edu>
2023-04-05 19:59:00 -0600

    app clean up

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { auth } from './FirebaseConfig';
-// import Login from './Login';
 import Social from './Social';
 import Name from './Name';
 import Gallery from './Gallery';
 import Schedule from './Schedule';
 
 function App() {
-  // const [user, setUser] = useState(null);
-
-  // useEffect(() => {
-  //   const unsubscribe = auth.onAuthStateChanged((user) => {
-  //     setUser(user);
-  //   });
-
-  //   return () => unsubscribe();
-  // }, []);
   const [firstName, setFirstName] = useState(""); 
   const [lastName, setLastName] = useState("");
   const [name, setName] = useState(false);
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={user ? <Social /> : <Navigate to="/login" />} />
-        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} /> */}
         <Route path='/' element={name ? <Social name={name} /> : < Name firstName={firstName} setFirstName={setFirstName} lastName={lastName} setLastName={setLastName} name={name} setName={setName}/> } />
         <Route path='/Gallery' element={<Gallery/>}/>
         <Route path='/Schedule' element={<Schedule/>}/>
