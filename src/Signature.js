81a39629ae737e47d896bc00d4ce753a291bcc6d
Jordan Strande <jstrande@rams.colostate.edu>
2023-04-10 17:19:33 -0600

    made signature easier to read

--- src/Signature.js
+++ src/Signature.js
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function Signature(){
     return (
-        <p style={{textAlign: "center", fontSize: "12px"}}>Crafted with love by Jordan Strande</p>
+        <p 
+            style={
+                {textAlign: "center", 
+                fontSize: "12px"}
+            }
+        >
+            Crafted with love by Jordan Strande
+        </p>
     )
 }
