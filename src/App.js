0f6c721c8a32e6d6c451f070f5bc30ef0d0417b7
Jordan Strande <jstrande@rams.colostate.edu>
2023-04-18 21:17:30 -0600

    Passed nessisary props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Social from './Social';
 import Name from './Name';
 import Gallery from './Gallery';
@@ -13,9 +13,48 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={name ? <Social name={name} setName={setName} timer={timer} setFirstName={setFirstName} setLastName={setLastName}/> : < Name firstName={firstName} setFirstName={setFirstName} lastName={lastName} setLastName={setLastName} name={name} setName={setName} setTimer={setTimer}/> } />
-        <Route path='/Gallery' element={name && <Gallery setTimer={setTimer}/>}/>
-        <Route path='/Schedule' element={name && <Schedule setTimer={setTimer}/>}/>
+
+        <Route path='/' element={
+          name ? 
+            <Social 
+              name={name}
+              setName={setName} 
+              timer={timer} 
+              setFirstName={setFirstName} 
+              setLastName={setLastName} 
+            /> : 
+            < Name 
+              firstName={firstName} 
+              setFirstName={setFirstName} 
+              lastName={lastName} 
+              setLastName={setLastName} 
+              name={name} 
+              setName={setName} 
+              setTimer={setTimer}/> 
+          } />
+
+        <Route path='/Gallery' element={
+          name ? 
+            <Gallery 
+              setTimer={setTimer} 
+              setName={setName} 
+              setFirstName={setFirstName} 
+              setLastName={setLastName}
+            /> : 
+            <Navigate to="/" />
+        }/>
+
+        <Route path='/Schedule' element={
+          name ? 
+            <Schedule 
+              setTimer={setTimer} 
+              setName={setName} 
+              setFirstName={setFirstName} 
+              setLastName={setLastName}
+            /> : 
+            <Navigate to="/" />
+        }/>
+        
       </Routes>
     </Router>
 
