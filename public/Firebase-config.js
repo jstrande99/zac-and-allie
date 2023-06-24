7e460ee1989622d5aa1bb82508fe46d3ba2c70a2
Jordan Strande <jstrande@rams.colostate.edu>
2023-06-24 08:07:31 -0600

    added config

---
+++ public/Firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4SrgxULcLC8pa5oz2wSlI6DOUhVC_kEQ",
+  authDomain: "zacandallie-ab1dd.firebaseapp.com",
+  projectId: "zacandallie-ab1dd",
+  storageBucket: "zacandallie-ab1dd.appspot.com",
+  messagingSenderId: "418662880538",
+  appId: "1:418662880538:web:db6999cafdd18ec70f08a3",
+  measurementId: "G-TKMMJJLDH3"
+};
