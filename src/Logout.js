d12192b2d8b3d927e5fe0c0b02265a3d9b54f3ec
Jordan Strande <jstrande@rams.colostate.edu>
2023-07-13 15:45:48 -0600

    moved logout

--- src/Logout.js
+++
@@ -1,7 +0,0 @@
-export function Logout(props){
-    localStorage.removeItem('name');
-	localStorage.removeItem('firstTimeLogIn');
-    props.setFirstName("");
-    props.setLastName("");
-    props.setName(null);
-}
